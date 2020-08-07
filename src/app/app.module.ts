1cd0f7e2214c718028df276781cd12c58883aac2
SDen4 <asw325@yandex.ru>
2020-08-07 19:40:32 +0500

    filters markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
+import { FiltersComponent } from './filters/filters.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent
+    ItemComponent,
+    FiltersComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
