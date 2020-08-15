add9d9140399759df2e1ff8feaf3802d12e6b2d3
SDen4 <asw325@yandex.ru>
2020-08-15 14:50:48 +0500

    pagination's arrows are working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UsersService } from "./users.service";
+import { TotalService } from "./total.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [UsersService]
+  providers: [UsersService, TotalService]
 })
 
 export class AppComponent implements OnInit {
@@ -15,20 +16,40 @@ export class AppComponent implements OnInit {
   public index = '';
   public searchStr = '';
   public usersLength: number;
+
+  public usersTotalLength: Array<any>;
+
   public page: number = 1;
   public users: Array<any>;
   public itemsPerPage: number = 5;
   public pages: Array<any> = [1,2]; // temp!!!!!!
+  private idx: number = 1;
 
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService, private totalService: TotalService) {
     this.loadPage();
-    console.log(this.page)
   }
 
   pageChange(idx: number, event: any) {
     event.preventDefault();
+    this.idx = idx+1;
     this.page = idx+1;
     this.loadPage();
+  }
+
+  pageChangeNext(event: any) {
+    event.preventDefault();
+    if(this.page < this.pages.length) {
+      this.page = this.page + 1;
+      this.loadPage();
+    }
+  }
+
+  pageChangePrev(event: any) {
+    event.preventDefault();
+    if(this.page > 1) {
+      this.page = this.page - 1;
+      this.loadPage();
+    }
   }
 
   onPageChanged(e:any) {
@@ -37,11 +58,20 @@ export class AppComponent implements OnInit {
 
   private loadPage(){
     this.users = this.usersService.users
+    // console.log(this.users);
+
     this.usersService.getUsers(this.page, this.itemsPerPage).subscribe(page => {
       this.users = page.rows;
       console.log(this.users);
       this.usersLength = page.totalCount;
     })
+
+    // this.usersTotalLength = this.totalService.superTotalCount;
+    this.totalService.getTotalUsers().subscribe(l => {
+      this.usersTotalLength = l;
+    });
+    console.log(this.usersTotalLength);
+
   }
 
   receiveFromChild(e: Array<any>){
